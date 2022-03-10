0bc152bedf26d6a26e91456d125dfed734d79183
Thulani <nalebaniokiy99@gmail.com>
2022-03-10 04:43:21 -0800

    se

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-putsm('Programming is like building a multilingual puzzle')
 int main(void)
 {
+puts('Programming is like building a multilingual puzzle')
     return (0);
 }
