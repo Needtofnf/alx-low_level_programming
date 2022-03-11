39de065b6596727388104ccdc9539fcf00c0abd3
Thulani <nalebaniokiy99@gmail.com>
2022-03-11 07:28:48 -0800

    see

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) 
+
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
+
+{
+    
+char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+    return (0);
+    
+}
