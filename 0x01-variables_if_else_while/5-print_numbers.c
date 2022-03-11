15159962ee7e102e3920279a2440a5e4ccb0d253
Thulani <nalebaniokiy99@gmail.com>
2022-03-11 07:12:47 -0800

    see

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
 /**
-  * main - Prints some letters of alphabet
+  * main - Prints the numbers since 0 to 9
   *
   * Return: Always (Success)
   */
 int main(void)
 {
-	char c;
+	char n;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		if (c != 'e' && c != 'q')
-		{
-			putchar(c);
-		}
+		putchar(n);
 	}
 
 	putchar('\n');
 
 	return (0);
 }
+
