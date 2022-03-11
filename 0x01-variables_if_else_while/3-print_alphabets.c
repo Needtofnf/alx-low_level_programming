c8524b35a98e0fdc8da37ca93f67ab538938fcc7
Thulani <nalebaniokiy99@gmail.com>
2022-03-11 05:57:48 -0800

    see

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,15 +2,20 @@
 
 int main() {
   
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
  char i;
 
 
     for(i='a'; i<='z'; i++)
-        printf("%c",i);
+       p utchar("%c",i);
  
  
     for(i='A'; i<='Z'; i++)
-        printf("%c",i); 
+       putchar("%c",i); 
         
         
     return 0;
