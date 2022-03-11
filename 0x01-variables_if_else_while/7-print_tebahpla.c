6cca1f5cd9a898ca7e8efdfac94ef8172965fa80
Thulani <nalebaniokiy99@gmail.com>
2022-03-11 11:22:11 -0800

    see

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+
+int main(void) 
+{
+    char a;
+    
+
+        for (a = 'z'; a <= 'a'; a++)
+        {
+                putchar(a);
+        }
+
+
+    putchar('\n');
+
+    return (0);
+    
+}
