b40406616057d031968966949aec452b476c2055
Thulani <nalebaniokiy99@gmail.com>
2022-03-11 04:37:44 -0800

    d

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,5 +10,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+
+	 int c;
+  int n;
+   printf("Ten random numbers in [1,100]\n");
+
+  for (c = 1; c <= 10; c++) {
+    n = rand() % 100 + 1;
+    printf("%d\n", n);
+  }
 	return (0);
 }
