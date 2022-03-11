50b5fd8a5964eeb4b5ee81660d8b2ab74dac22a8
Thulani <nalebaniokiy99@gmail.com>
2022-03-11 06:57:21 -0800

    see

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+  
+ #include <stdio.h>
+
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
+
+
+	char c;
+   
+	for (c = 'a'; c <= 'z';c++)
+	
+	if (c != 'e' && c != 'q')
+
+	{   
+	   
+		putchar(c);
+	}
+
+
+
+	putchar('\n');
+
+	
+
+    return 0;
+}
