b1d5bb4466cbc18ab33c4510b4727ac81e6b7d45
Thulani <nalebaniokiy99@gmail.com>
2022-03-15 10:06:44 -0700

    see

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,1 @@
-#include "main.h"
-/**
- * _putchar
- * Return: 0 on success.
- */
 int main(void)
-{
-	char c[] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	
-	}
-	_putchar('\n');
-	return (0);
-}
