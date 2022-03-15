3602f0ceb1903e4eb7c2e674fd94af4fce1f5f0d
Thulani <nalebaniokiy99@gmail.com>
2022-03-15 12:44:28 -0700

    see

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,15 @@
 #include "main.h"
-
 /**
- * main - entry point
- *
- * Description: Prints _putchar  with _putchar
- *
- * Return: always 0
- */
-
-int main(void)
+* print_alphabet - Entry point
+* Return:0
+*/
+void print_alphabet(void)
 {
-	int i = 0;
-        char c;
-        char s[] = "_putchar\n";
-
-	while (i <= 9)
-	{
-		c = s[i];
-		_putchar(c);
-		i++;
-	}
-	return (0);
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
 }
