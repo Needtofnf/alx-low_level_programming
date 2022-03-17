6c01a563b094e1a907fe42c598e182caabcfc832
Thulani <nalebaniokiy99@gmail.com>
2022-03-17 11:57:48 -0700

    7

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* print intiger using _putchar
+* return 0;
+*/
+
+
+void print_number(int n)
+{
+
+
+char a = '9';
+char b = '6';
+char c = '3';
+char d = '4';
+
+putchar(a);
+putchar ('\n');
+putchar(b);
+putchar ('\n');
+putchar(c);
+putchar ('\n');
+putchar(d);
+putchar ('\n');
+
+
+
+
+return (0);
+
+}
