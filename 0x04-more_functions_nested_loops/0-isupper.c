f1e2149be9afd1c1b8ff1885ba487836dba8669a
Thulani <nalebaniokiy99@gmail.com>
2022-03-17 03:54:06 -0700

    bettty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code.
- *
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
  * Return: Always 0.
  */
-
 int _isupper(int c)
 {
 
