dd2bd703e67c77e4a809f1703635093f461b9373
Thulani <nalebaniokiy99@gmail.com>
2022-03-22 06:01:32 -0700

    see

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n){
+
+
+    int  *ptr = "98";
+    printf("The value of ptr is : %x\n", ptr);
+
+    return (0);
+}
+
