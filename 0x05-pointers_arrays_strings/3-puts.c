53e1136671e5ae7813e9408e333e7c9879b11188
Thulani <nalebaniokiy99@gmail.com>
2022-03-22 13:01:38 -0700

    see

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+* DESCRIPTION : Write a function that prints a string, followed by a new line, to stdout.
+*
+*
+*/
+void _puts(char *str) 
+{
+    
+    char wen = "string";
+    
+        _putchar("wen");
+        
+        return (0); 
+}   
