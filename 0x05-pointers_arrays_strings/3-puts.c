c048e774a5370cd8dbfde4d1ff40f10184332131
Thulani <nalebaniokiy99@gmail.com>
2022-03-22 13:36:47 -0700

    see

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * _puts - prints a string followed by a new line..
  *
- * Return: Always 0.
+ *@str: the string to print.
+ *
  */
-int main(void)
-{
-   char *str;
 
-   str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
