d9f9d79e1b9ebffe0df29b71bb80ffaffb3402fa
Thulani <nalebaniokiy99@gmail.com>
2022-03-24 12:00:32 -0700

    see

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src)
+ {
+
+
+
+        char number1[] = "src";
+        char number2[] = "dest";
+
+         printf ("The output: %s", strcat(number1, number2));
+
+
+
+   return (dest);
+}
+
