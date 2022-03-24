8891609da4f6f8841b5b9ee646e8b36abb0d5759
Thulani <nalebaniokiy99@gmail.com>
2022-03-24 12:04:04 -0700

    see

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,27 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ *_strcat - concatinate 2 strings.
+ *@dest: first string.
+ *@src: second string.
+ *Return: string.
+ */
 
 char *_strcat(char *dest, char *src)
- {
+{
+	int i = 0, j = 0;
 
+	while (dest[i] != '\0')
+		i++;
 
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-        char number1[] = "src";
-        char number2[] = "dest";
+	dest[i] = '\0';
 
-         printf ("The output: %s", strcat(number1, number2));
-
-
-
-   return (dest);
+	return (dest);
 }
-
