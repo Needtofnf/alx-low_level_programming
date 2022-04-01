fca414a985f97b35abe1f0d7f9530c6c2e843d71
Thulani <nalebaniokiy99@gmail.com>
2022-04-01 07:48:17 -0700

    ser

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,20 @@
 
 int main() {
     // Write C code here
-    printf("Hello world program \n ");
+    char program_name[100] ;
+    int a ;
     
+
+    
+    for (a =0; a<= 4; a++)
+        {
+            
+       printf("Program name is\n");
+       scanf ("%s", &program_name);
+       printf("Yor new program name is : %s \n", program_name);
+            
+        }
+        
+        
     return 0;
 }
