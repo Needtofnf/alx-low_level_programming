ee62ef5f3047e91a6512a41d064bb088a7963656
Thulani <nalebaniokiy99@gmail.com>
2022-04-01 10:57:37 -0700

    sec

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,33 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
-
-/**
- * _isnumber - checks if string is a number
- * @s: string
- *
- * Return: On success 1.
- * If not a number, 0 is returned.
- */
-int _isnumber(char *s)
-{
-	int i, check, d;
-
-	i = 0, d = 0, check = 1;
-	if (*s == '-')
-		i++;
-	for (; *(s + i) != 0; i++)
-	{
-		d = isdigit(*(s + i));
-		if (d == 0)
-		{
-			check = 0;
-			break;
-		}
-	}
-	return (check);
-}
 /**
  * main - Entry point
  *
@@ -37,36 +10,14 @@ int _isnumber(char *s)
  */
 int main(int argc, char **argv)
 {
-	int j, ex, coins, cents, d;
-	int c[5] = {25, 10, 5, 2, 1};
+	int i;
 
-	ex = 1, j = 0, coins = 0;
-	if (argc == 2)
+	if (argc > 0)
 	{
-		if (_isnumber(argv[1]))
+		for (i = 0; i < argc; i++)
 		{
-			ex = 0, cents = atoi(argv[1]);
-			if (cents >= 0)
-			{
-				while (cents != 0)
-				{
-					d = cents / c[j];
-					if (d == 0)
-					{
-						j++;
-					}
-					else
-					{
-						coins += d;
-						cents -= (d * c[j]);
-					}
-				}
-			}
+			printf("%s\n", argv[i]);
 		}
 	}
-	if (ex == 0)
-		printf("%i\n", coins);
-	else
-		printf("%s\n", "Error");
-	return (ex);
+	return (0);
 }
