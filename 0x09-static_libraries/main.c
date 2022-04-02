81ac37ab4cd8b78fc3b201ba2b7ffead4dbffe0d
Thulani <nalebaniokiy99@gmail.com>
2022-04-02 15:21:37 -0700

    asassasassasa

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 _puts("\"At the end of the day, my goal was");
-_puts(to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("to be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
 
