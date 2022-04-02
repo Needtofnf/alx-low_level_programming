79553a4411e46e99ae78052022d154420bdc36ba
Thulani <nalebaniokiy99@gmail.com>
2022-04-02 15:48:21 -0700

    ser

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,14 +1,1 @@
-#include "main.h"
-/**
-* main -Static Librairies.
-*
-* Return: Always 0.
-*/
-
-int main(void)
-{
-_puts("\"At the end of the day, my goal was");
-_puts("to be the best hacker\"\n\t- Kevin Mitnick");
-return (0);
-}
 
