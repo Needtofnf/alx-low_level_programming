2feee124bd85f5ef0169b6c5591798dd79432e77
Thulani <nalebaniokiy99@gmail.com>
2022-04-02 14:39:43 -0700

    we_done

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,11 @@
-int main(void)
+#include "main.h"
+/**
+* main -Static Librairies.
+*
+* Return: Always 0.
+*/
 
+int main(void)
 {
 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
