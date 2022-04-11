f337f8fc6723cb80a3482efcb896bb7216c909e7
Thulani <nalebaniokiy99@gmail.com>
2022-04-11 05:50:22 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,24 @@
+#ifndef foo_H
+#define foo_H
+
+#include <stdio.h>
+/*
+ *
+ * DESCRIPTIO: function main  prints 2 -main.c
+ *
+ * return 0;
+ */
+
+int main(void)
+{
+
+printf("2-main.c\n");
+
+
+
+
+return (0);
+}
+
+
+#endif
