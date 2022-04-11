c44ab0908163112e7c30b502a91d99f2e0a869e9
Thulani <nalebaniokiy99@gmail.com>
2022-04-11 05:26:32 -0700

    1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,17 +1,3 @@
 #include <stdio.h>
 #define PI 3.14159265359
 
-int main(void)
-{
-
-float radius, area;
-printf("Enter the radius: ");
-
-scanf("%f", &radius);
-
-area = PI* radius* radius;
-
-printf("Area=%.2f", area)
-	;
-return (0);
-}
