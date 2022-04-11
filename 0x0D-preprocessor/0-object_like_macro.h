ac1336cb3b83e67098f3f1a87f61045571023a6c
Thulani <nalebaniokiy99@gmail.com>
2022-04-11 05:02:48 -0700

    ty

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,19 +2,5 @@
 #define foo_H
 
 #define BUFFER_SIZE 1024
-#include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int s;
-int SIZE;
-s = 98 + SIZE;
-printf("%d\n", s);
-return (0);
 
-}
 #endif
