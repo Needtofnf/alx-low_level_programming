bb3b62ac78e53002813cbb2a5516fcb858d8cc49
Thulani <nalebaniokiy99@gmail.com>
2022-04-13 08:42:13 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+
+    printf("The name is: %s\n", name);
+
+
+}
+
+int main ()
+{
+    print_name("bongaa");
+
+
+    retun (0);
+}
+
