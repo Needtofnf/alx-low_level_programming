6d7d57ccfba52e28f528641c5563ca9aca9fb629
Thulani <nalebaniokiy99@gmail.com>
2022-04-14 02:46:59 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+
+if (n == 0)
+
+{
+return (0);
+}
+
+}
