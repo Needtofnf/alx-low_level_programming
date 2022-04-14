40c2ad57f7737f1cba037c8681fa0545e95b6eb6
Thulani <nalebaniokiy99@gmail.com>
2022-04-14 10:48:53 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,34 +1,38 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
-
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
-  va_list ap;
-  int i, sum;
+va_list ap;
+int i, sum;
 
-  va_start (ap, n);         /* Initialize the argument list. */
+va_start(ap, n);         /* Initialize the argument list. */
 
-  sum = 0;
-  for (i = 0; i < n; i++)
-    sum += va_arg (ap, const unsigned int);    /* Get the next argument value. */
+sum = 0;
+for (i = 0; i < n; i++)
+sum += va_arg(ap, const unsigned int);    /* Get the next argument value. */
 
-  va_end (ap);                  /* Clean up. */
-  return (sum);
+va_end(ap);                  /* Clean up. */
+return (sum);
 }
 
 
-int main (void)
+int main(void)
 {
-    int n ;
-  /* This call prints 16. */
-  printf ("%d\n", sum_them_all (2, 98, 1024));
-  printf ("%d\n", sum_them_all (4, 98, 1024, 402, -1024));
- if (n == 0)
- {
-     return (0);
- }
+int n;
+/* This call prints 16. */
+printf("%d\n", sum_them_all(2, 98, 1024));
+printf("%d\n", sum_them_all(4, 98, 1024, 402, -1024));
+if (n == 0)
+{
+return (0);
+}
 
 }
 
